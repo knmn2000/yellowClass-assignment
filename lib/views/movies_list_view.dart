0a2f7c4ec8209477a8764a2c5bb2ad571da627e4
karan <karan@radicali.com>
2021-08-14 13:25:47 +0530

    fix listview

--- lib/views/movies_list_view.dart
+++ lib/views/movies_list_view.dart
@@ -7,11 +7,13 @@ import 'package:http/http.dart' as http;
 class MoviesListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map<String, dynamic>>(
+    // return FutureBuilder<Map<String, dynamic>>(
+    return FutureBuilder<List<Movie>>(
       future: _fetchMovies(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          Map<String, dynamic> data = snapshot.data;
+          // Map<String, dynamic> data = snapshot.data;
+          List<Movie> data = snapshot.data;
           return _moviesListView(data);
         } else if (snapshot.hasError) {
           return Text("${snapshot.error}");
@@ -21,31 +23,29 @@ class MoviesListView extends StatelessWidget {
     );
   }
 
-  // Future<List<Movie>> _fetchMovies() async {
-  Future<Map<String, dynamic>> _fetchMovies() async {
+  Future<List<Movie>> _fetchMovies() async {
+    // Future<Map<String, dynamic>> _fetchMovies() async {
     final moviesListAPIUrl =
         'https://api.themoviedb.org/3/discover/movie?api_key=bb058074a670ad43d29e1d396c92ef1f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
     final response = await http.get(Uri.parse(moviesListAPIUrl));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> jsonResponse = json.decode(response.body);
-      // jsonResponse.
-      return jsonResponse
-          .forEach((movie, idx) => new Movie.fromJson(movie))
-          .toList();
+      List data = jsonResponse['results'];
+      return data.map((movie) => new Movie.fromJson(movie)).toList();
     } else {
       throw Exception('Error');
     }
   }
 
-  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+  ListTile _tile(String title, String subtitle, String poster) => ListTile(
         title: Text(title,
             style: TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 20,
             )),
         subtitle: Text(subtitle),
-        leading: Icon(icon, color: Colors.blue[500]),
+        // leading: Icon(icon, color: Colors.blue[500]),
       );
 
   ListView _moviesListView(data) {
